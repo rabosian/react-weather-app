ee43b7a9d6d3b89c4f3f80e6b868498f95926819
rabosian <msann1356@gmail.com>
2022-07-27 15:42:08 -0400

    add error handling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Alert } from "@mui/material";
 import DisplayBox from "./component/DisplayBox";
 import DisplayButton from "./component/DisplayButton";
-
-// 1. as soon as app starts, displaying the weather of current location
-// 2. data includes location(city) name, weather, status
-// 3. control panel contains 5 cities (current location, seoul, vancouver, paris, new york)
-// 4. every fetching process includes loading spinner
 
 function App() {
   const [weather, setWeather] = useState();
   const [loading, setLoading] = useState(true);
   const [currentCity, setCurrentCity] = useState("");
+  const [error, setError] = useState("");
 
-  const cities = ["Toronto", "vancouver", "seoul", "paris", "new york"];
+  const cities = ["Toronto1", "vancouver", "seoul", "paris", "new york"];
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude;
-      let lon = position.coords.longitude;
-      getWeatherByCurrentLocation(lat, lon);
+      const { latitude, longitude } = position.coords
+      getWeatherByCurrentLocation(latitude, longitude);
     });
   };
 
-  const getWeatherByCityName = async (cityname) => {
+  const getWeatherByCityName = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
-      setLoading(true);
+      setError('')
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
       let response = await fetch(url);
       let data = await response.json();
       setWeather(data);
     } catch (e) {
-      console.log(e.message);
+      setError(e.message);
     }
     setLoading(false);
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     try {
+      setError('')
       let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
-      setLoading(true);
       let response = await fetch(url);
       let data = await response.json();
       setWeather(data);
     } catch (e) {
-      console.log(e.message);
+      setError(e.message);
     }
     setLoading(false);
   };
 
   useEffect(() => {
+    setLoading(true)
     if (!currentCity) {
       getCurrentLocation();
     } else {
-      getWeatherByCityName(currentCity);
+      getWeatherByCityName();
     }
   }, [currentCity]);
 
@@ -70,6 +66,7 @@ function App() {
     <div className="App">
       <div className="background">
         <div className="container">
+          {error && <Alert severity="error">error</Alert>}
           {loading ? <CircularProgress /> : <DisplayBox data={weather} />}
           <DisplayButton
             cities={cities}
